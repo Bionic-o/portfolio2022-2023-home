15a44e9f3c269819b0a85170ce1e6b5fea165271
Bionic-o <weiskopf.bionico@gmail.com>
2022-12-11 14:43:17 +0100

    remove underline and color of links in dropdown menu

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -15,11 +15,11 @@ function DropdownMenu () {
         <img id="menu" src="/images/menu.png" alt="menu" onMouseOver={showDropdown} onMouseLeave={hideDropdown}></img>
             {state ? (
                 <dl id="dropdown_list" onMouseEnter={showDropdown}>
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/projects"><li>Projekte / Projects</li></Link>
-                    <Link to="/cv"><li>Lebenslauf / CV</li></Link>
-                    <Link to="/skills"><li>Fähigkeiten / Skills</li></Link>
-                    <Link to="/aboutme"><li>Über Mich / About Me</li></Link>
+                    <Link className="link_decoration" to="/"><li>Home</li></Link>
+                    <Link className="link_decoration" to="/projects"><li>Projekte / Projects</li></Link>
+                    <Link className="link_decoration" to="/cv"><li>Lebenslauf / CV</li></Link>
+                    <Link className="link_decoration" to="/skills"><li>Fähigkeiten / Skills</li></Link>
+                    <Link className="link_decoration" to="/aboutme"><li>Über Mich / About Me</li></Link>
                 </dl>
             ): null}
         </div>
