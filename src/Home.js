e11bfe0c0aaa6b82769785236a467bff60d9face
Bionic-o <weiskopf.bionico@gmail.com>
2023-05-02 09:13:54 +0200

    Key-Qualifications home

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,16 @@ import Bewerbungsfoto from "./assets/Bewerbungsfoto00.jpg"
 function Home () {
     return (
         <div id="Home">
-            <img id="Bewerbungsfoto" src={Bewerbungsfoto} alt="Mypic"></img>
+                <img id="Bewerbungsfoto" src={Bewerbungsfoto} alt="Mypic"></img>
+                <div>
+                    <p id="Key_Skills">Key-Qualifications:</p>
+                    <ul id="home_list">
+                        <li>C#</li>
+                        <li>HTML, CSS, JS, React</li>
+                        <li>ERP Systems (Wilkens, SAP)</li>
+                        <li></li>
+                    </ul>
+                </div>
         </div>
     )
 }
